e6134363d2c13cb88dcf8181f1159d4e08f85a0a
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-20 19:13:38 +0700

    Remove user

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  attr_accessor :name, :email
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
-end

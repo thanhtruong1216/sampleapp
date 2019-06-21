224a80c58b06be3319076d7f4e09549fc8aff952
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-21 09:19:06 +0700

    fix: generate model user again

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_101824) do
+ActiveRecord::Schema.define(version: 2019_06_20_121343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.text "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
